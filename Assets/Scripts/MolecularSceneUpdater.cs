45ef3e2b55574dadec008cde0e41002c0d1a3cee
David Kouřil <david.kouril@gmail.com>
2016-04-04 14:16:36 +0200

    rotation loading fixed.

--- Assets/Scripts/MolecularSceneUpdater.cs
+++ Assets/Scripts/MolecularSceneUpdater.cs
@@ -3,6 +3,10 @@ using System.Collections;
 using System.Runtime.InteropServices; // because of DllImport
 using System; // because of IntPtr
 
+/*
+    In Update(), this class loads data from Maya scene via shared memory
+*/
+
 //[ExecuteInEditMode]
 public class MolecularSceneUpdater : MonoBehaviour {
 
@@ -35,6 +39,10 @@ public class MolecularSceneUpdater : MonoBehaviour {
     {
 
         //Debug.Log("MolecularSceneUpdater: Update()");
+        // some debug shit:
+        //GameObject cube = GameObject.Find("Cube");
+        //Quaternion q = cube.transform.rotation;
+
 
         float[] shMemContent = new float[256];
 
@@ -43,21 +51,51 @@ public class MolecularSceneUpdater : MonoBehaviour {
         int numObjsLoaded = 3;
         for (int i = 0; i < numObjsLoaded; ++i)
         {
-            var x = shMemContent[i*3 + 0];
-            var y = shMemContent[i*3 + 1];
-            var z = shMemContent[i*3 + 2];
+            var pos_x = shMemContent[i*7 + 0];
+            var pos_y = shMemContent[i*7 + 1];
+            var pos_z = shMemContent[i*7 + 2];
+            pos_z *= -1;                            // position conversion from right handed (maya) coordinate system
+
+            var rot_x = shMemContent[i*7 + 3];
+            var rot_y = shMemContent[i*7 + 4];
+            var rot_z = shMemContent[i*7 + 5];
+            var rot_w = shMemContent[i*7 + 6];
 
             GameObject cb = GameObject.Find("Cube" + i);
             if (cb == null)
             { // game object for this object has not been yet created
-                //cb = new GameObject("Cube" + i);
                 cb = GameObject.CreatePrimitive(PrimitiveType.Cube);
                 cb.name = "Cube" + i;
-                cb.transform.position = new Vector3(x, y, z);
-            } else
-            {
-                cb.transform.position = new Vector3(x, y, z);
             }
+
+            // debugging just the first instance
+            if (cb.name == "Cube0")
+            {
+                //Debug.Log("Cube0 position: " + pos_x + " " + pos_y + " " + pos_z);
+                //Debug.Log("Cube0 rotation quaternion = (" + rot_x + " " + rot_y + " " + rot_z + " " + rot_w + ")");
+                //float x = cb.transform.rotation.eulerAngles.x;
+                //float y = cb.transform.rotation.eulerAngles.y;
+                //float z = cb.transform.rotation.eulerAngles.z;
+                //Debug.Log("loaded rotation: (" + x + ", " + y + ", " + z + ")");
+            }
+            
+            Quaternion rot = new Quaternion(rot_x, rot_y, rot_z, rot_w);
+            Vector3 rotDeg = rot.eulerAngles;
+
+            // this way it works for a single axis rotations:
+            Quaternion x = Quaternion.AngleAxis(-rotDeg.x, Vector3.right);
+            Quaternion y = Quaternion.AngleAxis(-rotDeg.y, Vector3.up);
+            Quaternion z = Quaternion.AngleAxis(rotDeg.z, Vector3.forward);
+            //rot = x * y * z;      // rotation around x changes direction midway
+            //rot = x * z * y;      // same
+            rot = y * x * z;      // wow, this looks like this is it
+            //rot = y * z * x;      // direction change
+            //rot = z * x * y;      // weird combined rotations
+            //rot = z * y * x;        // direction change
+
+            // setting the transoform on an object
+            cb.transform.position = new Vector3(pos_x, pos_y, pos_z);
+            cb.transform.localRotation = rot;
         }
 
     }
